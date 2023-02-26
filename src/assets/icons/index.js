db24d7fd6e679ee1809bc978c74d8e01daf32ca2
ChuckBDT <charlesboudrant@protonmail.com>
2023-02-26 13:07:52 +0100

    Main structure

---
+++ src/assets/icons/index.js
@@ -0,0 +1,6 @@
+import CyclingMan from "./CyclingMan";
+import Dumbbell from "./Dumbbell";
+import SeatedMan from "./SeatedMan";
+import SwimmingMan from "./SwimmingMan";
+
+export { CyclingMan, SwimmingMan, Dumbbell, SeatedMan };
