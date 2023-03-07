d7da0c25110b96f4e9cb0503f2918bd7c1108a72
ChuckBDT <charlesboudrant@protonmail.com>
2023-03-07 17:15:53 +0100

    ActivityChartTweak optimized

--- src/service/activityChartTweak.js
+++ src/service/activityChartTweak.js
@@ -1,11 +1,7 @@
 export default function activityChartTweak(data) {
-  const treatedData = [];
   for (let el of data) {
-    treatedData.push({
-      day: new Date(el.day).getDate(),
-      kilogram: el.kilogram,
-      calories: el.calories,
-    });
+    el.day = new Date(el.day).getDate();
   }
-  return treatedData;
+
+  return data;
 }
