428d4f4c507808e977dc1b1f164b1a874b34e313
ChuckBDT <charlesboudrant@protonmail.com>
2023-03-09 14:13:42 +0100

    Service enhanced

--- src/service/service.js
+++ src/service/service.js
@@ -2,33 +2,30 @@ import modelling from "./modelling/modelling";
 
 export default async function fetchUsersData(id, mocked) {
   let user, activity, averageSessions, performance;
+  let [userRes, activityRes, averageSessionsRes, performanceRes] = [];
 
   if (mocked) {
-    const [userRes, activityRes, averageSessionsRes, performanceRes] =
+    [userRes, activityRes, averageSessionsRes, performanceRes] =
       await Promise.all([
         fetch(`./mockedData/${id}-id.json`),
         fetch(`./mockedData/${id}-activity.json`),
         fetch(`./mockedData/${id}-average-sessions.json`),
         fetch(`./mockedData/${id}-performance.json`),
       ]);
-
-    user = await userRes.json();
-    activity = await activityRes.json();
-    averageSessions = await averageSessionsRes.json();
-    performance = await performanceRes.json();
   } else {
-    const [userRes, activityRes, averageSessionsRes, performanceRes] =
+    [userRes, activityRes, averageSessionsRes, performanceRes] =
       await Promise.all([
         fetch(`http://localhost:3456/user/${id}`),
         fetch(`http://localhost:3456/user/${id}/activity`),
         fetch(`http://localhost:3456/user/${id}/average-sessions`),
         fetch(`http://localhost:3456/user/${id}/performance`),
       ]);
-
-    user = await userRes.json();
-    activity = await activityRes.json();
-    averageSessions = await averageSessionsRes.json();
-    performance = await performanceRes.json();
   }
+
+  user = await userRes.json();
+  activity = await activityRes.json();
+  averageSessions = await averageSessionsRes.json();
+  performance = await performanceRes.json();
+
   return modelling([user, activity, averageSessions, performance]);
 }
